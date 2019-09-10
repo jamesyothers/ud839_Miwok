1a8ba568c896395fc37adcb576b9c10d3544e040
jamesyothers <jamesjsdev@gmail.com>
2019-09-09 19:07:11 -0500

    feat: add icons to list view

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,10 +4,17 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceID;
 
     public Word (String DefaultTranslation, String MiwokTranslation) {
         mDefaultTranslation = DefaultTranslation;
         mMiwokTranslation = MiwokTranslation;
+    }
+
+    public Word (String DefaultTranslation, String MiwokTranslation, int ImageResourceID) {
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = MiwokTranslation;
+        mImageResourceID = ImageResourceID;
     }
 
     public String getMiwokTranslation (){
@@ -16,6 +23,10 @@ public class Word {
 
     public String getDefaultTranslation (){
         return mDefaultTranslation;
+    }
+
+    public int getImageResourceID (){
+        return mImageResourceID;
     }
 
 }
