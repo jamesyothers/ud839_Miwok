054d3496121b64bc2be26e85978c03bfb33c7841
jamesyothers <jamesjsdev@gmail.com>
2019-09-14 15:09:06 -0500

    bugfix: fix colors audio playback

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -36,10 +36,14 @@ public class FamilyActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
-        words.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
-        words.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("father", "әpә", R.drawable.family_father,
+                R.raw.family_father));
+        words.add(new Word("mother", "әṭa", R.drawable.family_mother,
+                R.raw.family_mother));
+        words.add(new Word("son", "angsi", R.drawable.family_son,
+                R.raw.family_son));
+        words.add(new Word("daughter", "tune", R.drawable.family_daughter,
+                R.raw.family_daughter));
         words.add(new Word("older brother", "taachi", R.drawable.family_older_brother,
                 R.raw.family_older_brother));
         words.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother,
